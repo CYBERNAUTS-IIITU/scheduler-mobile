507ee625d3856d3f5864acbc21d4fabf0966dd4f
DevangSharma <debusharma1999@gmail.com>
2020-08-03 02:51:02 +0530

    Login screen Updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,28 @@
 import 'package:flutter/material.dart';
 import 'color.dart';
+import 'package:flutter/services.dart';
 
 void main() => runApp(LoginScreen());
 
 class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+      statusBarColor: clr1,
+    ));
+
     return MaterialApp(
       title: "Login",
+      theme: ThemeData(
+      ),
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: Colors.white,
-        appBar: AppBar(
-          title: Text("Login"),
-          backgroundColor: clr1,
-        ),
-
+        // appBar: AppBar(
+        //   title: Text("Login"),
+        //   backgroundColor: clr1,
+        // ),
         body: Center(
           child: SingleChildScrollView(
             child: Column(
@@ -29,46 +36,68 @@ class LoginScreen extends StatelessWidget {
                   width: 300,),
                   ),
 
-                ListTile(
-                  leading: Icon(Icons.person),
-                  title: TextFormField(
-                    validator: (input){
-                      if(input.isEmpty)
-                      {
-                        return "Invalid Username";
-                      }
-                    },
-                    decoration: InputDecoration(
-                      labelText: "Username",
+                SizedBox(
+                  width: 300,
+                  child: ListTile(
+                    leading: Icon(Icons.person),
+                    title: TextFormField(
+                      validator: (input){
+                        if(input.isEmpty)
+                        {
+                          return "Invalid Username";
+                        }
+                      },
+                      decoration: InputDecoration(
+                        labelText: "Username",
+                      ),
                     ),
                   ),
                 ),
 
-                ListTile(
-                  leading: Icon(Icons.lock),
-                  title: TextFormField(
-                    validator: (input){
-                      if(input.isEmpty)
-                      {
-                        return "Invalid Password";
-                      }
-                    },
-                    decoration: InputDecoration(
-                      labelText: "Password",
+                SizedBox(
+                  width: 300,
+                  child: ListTile(
+                    leading: Icon(Icons.lock),
+                    title: TextFormField(
+                      validator: (input){
+                        if(input.isEmpty)
+                        {
+                          return "Invalid Password";
+                        }
+                      },
+                      decoration: InputDecoration(
+                        labelText: "Password",
+                      ),
                     ),
                   ),
                 ),
 
                 Padding(
-                  padding: EdgeInsets.all(10.0),
-                  child: RaisedButton(
-                    child: Text("  Log In  "),
-                    onPressed:(){},
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(20.0),
-                    ),
-                    color: clr1,
-                  
+                  padding: EdgeInsets.fromLTRB(20, 20, 20, 10),
+                  child: SizedBox(
+                      width: 300,
+                      child: MaterialButton(
+                        child: Text("  Log In  ",
+                        style: TextStyle(
+                          fontSize: 20.0,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+
+                        ),),
+                        onPressed:(){},
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20.0),
+                        ),
+                        color: clr1,
+                      ),
+                  ),
+                ),
+
+                Padding(
+                  padding: EdgeInsets.fromLTRB(20, 0, 20, 20),
+                  child: GestureDetector(
+                    onTap: (){},
+                    child: Text("Forgot Password ? Click Here"),
                   ),
                 )
               ],
